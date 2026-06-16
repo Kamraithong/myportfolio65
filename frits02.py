793fef1cd74072bb5fbe6d86f65aaa5790412709
Kamraithong <164274@wsk.ac.th>
2026-06-16 04:45:49 +0000

    frits02.py

---
+++ frits02.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวนหาพื้นที่ของสามเหลี่ยม")
+h=float(input("enter hight"))
+b=float(input("enter base"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("ผู้จัดทำโดย นางสาว กำไรทอง สุขสราญ เลขที่ 29")
